2ae8bbd078214ac6ad0600536aa19292e97db664
chrisgrasmussen <46659042+chrisgrasmussen@users.noreply.github.com>
2024-06-23 14:49:04 -0700

    Django Rest Framework Added
    EC-1 : Create rest framework

---
+++ eidetic/start/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Start
+
+class StartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Start
+        fields = ('id', 'title', 'timestamp')
+    
