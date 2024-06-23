8fd2043e80c1f2ee7b39dafd372be197a80149c4
chrisgrasmussen <46659042+chrisgrasmussen@users.noreply.github.com>
2024-06-23 13:18:43 -0700

    Commit #1
    EC-12 : Get the project up on github

---
+++ eidetic/start/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Start(models.Model):
+    title = models.CharField(max_length=128)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ('-timestamp',)
+        verbose_name = ("Start")
+        verbose_name_plural = ("Starts")
+    
+    
+        
+
